88fe299aa31df76ef874e8503341528c6388bb00
Robert <robkowm+gdb@gmail.com>
2024-11-05 21:04:02 +0100

    bez ImageField

--- mydjango/Scripts/blog/models.py
+++ mydjango/Scripts/blog/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True,null=True)
-    image = models.ImageField(null=True, blank=True, upload_to='images/', default='images/car.png')
+    # image = models.ImageField(null=True, blank=True, upload_to='images/', default='images/car.png')
 
     def publish(self):
         self.published_date = timezone.now()
