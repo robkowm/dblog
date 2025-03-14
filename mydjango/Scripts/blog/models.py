26552a2d2c5f2dadebe1b1fc3a328da404a6a9f0
Robert <robkowm+gdb@gmail.com>
2025-03-14 16:05:35 +0100

    img na serwer - proba

--- mydjango/Scripts/blog/models.py
+++ mydjango/Scripts/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from PIL import Image
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
